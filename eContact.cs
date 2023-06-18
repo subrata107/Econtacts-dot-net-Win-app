adb712ee5d6f65ec54481beb828bebb82a86b52b
subrata107 <kundusubrata710@gmail.com>
2023-06-18 23:02:56 +0530

    Add project files.

---
+++ eContact.cs
@@ -0,0 +1,156 @@
+﻿using Econtacts.eContactClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.PeerToPeer.Collaboration;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Econtacts
+{
+    public partial class eContact : Form
+    {
+        
+        public eContact()
+        {
+            InitializeComponent();
+        }
+        
+        eContactClass eC = new eContactClass();
+        private void eContact_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            label6.Text = "";
+            label7.Text = "";
+            bool success=false;
+            eC.FirstName = textBox1.Text;     
+            eC.LastName = textBox2.Text;
+            eC.ContactNum = textBox3.Text;
+            eC.City = textBox4.Text;
+            
+            if (eC.FirstName != "" && eC.LastName !="" && eC.ContactNum !="" && eC.City !="" )
+            {
+                 success = eC.Insert(eC);
+                
+            }
+            else
+            {
+                
+                
+                label6.Text="Blank record Not Allowed for Insertion";
+            }
+
+            if (success == true)
+            {
+
+                label7.Text = "Records Inserted";
+
+                DataTable dt = eC.ShowAllRecords();
+                dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                MessageBox.Show(" Error inserting record");
+                label6.Text = "";
+            }
+           
+
+        }
+
+        private void ShowAllRecords_Click(object sender, EventArgs e)
+        {
+            DataTable dt = eC.ShowAllRecords();
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            label6.Text = "";
+            label7.Text = "";
+            bool success = false;
+            eC.FirstName = textBox1.Text;
+            eC.LastName = textBox2.Text;
+            eC.ContactNum = textBox3.Text;
+            eC.City = textBox4.Text;
+
+            if (eC.FirstName != "" && eC.LastName != "" && eC.ContactNum != "" && eC.City != "")
+            {
+                success = eC.Update(eC);
+
+            }
+            else
+            {
+                label6.Text = "Blank record Not Allowed for Updation";
+            }
+
+            if (success == true)
+            {
+                label7.Text = "Updation Successful";
+
+                DataTable dt = eC.ShowAllRecords();
+                dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                MessageBox.Show(" Error Updating record");
+                label6.Text = "";
+            }
+
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+
+        {
+            label6.Text = "";
+            label7.Text = "";
+            bool success = false;
+
+            eC.ContactNum = textBox3.Text;
+
+
+            if (eC.ContactNum != "")
+            {
+                success = eC.Delete(eC);
+
+            }
+            else
+            {
+                label6.Text = "Provide your Contact# to Delete";
+            }
+
+            if (success == true)
+            {
+                label7.Text = "Record Deleted Successfully";
+
+                DataTable dt = eC.ShowAllRecords();
+                dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                MessageBox.Show(" Error Deleting record");
+                label6.Text = "";
+            }
+
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            label6.Text = "";
+            label7.Text = "";
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+         
+    }
+}
---
+++ eContactClasses/eContactClasses.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Econtacts.eContactClasses
+{
+
+    internal class eContactClass
+    {
+        //public int Cust_id { get; set; }
+         public string FirstName { get; set; }
+         public string LastName { get; set; }
+         public string ContactNum { get; set; }
+         public string City { get; set; }
+
+
+         static string ConnString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+
+        
+        //reflecting data from db
+        public DataTable ShowAllRecords()
+        {
+
+            //Step:1 database connection
+            SqlConnection conn = new SqlConnection(ConnString);
+            DataTable dt = new DataTable();
+            try
+            {
+                //Writing SQL query
+                string sql = "SELECT * FROM CustomerDetails";
+
+                //creating CMD using Sql and conn
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Creating datadaptor using cmd
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(dt);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dt;
+        }
+
+        //inserting data into db
+        public bool Insert(eContactClass c)
+        {
+            bool isSuccess = false;
+            
+
+            SqlConnection conn = new SqlConnection(ConnString);
+            try
+            {
+                string sql = "INSERT INTO CustomerDetails (FirstName,LastName,ContactNum,City) VALUES (@FirstName,@LastName,@ContactNum,@City)";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@FirstName", c.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", c.LastName);
+                cmd.Parameters.AddWithValue("@ContactNum", c.ContactNum);
+                cmd.Parameters.AddWithValue("@City", c.City);
+
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                    return isSuccess;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+
+            }
+            return isSuccess;
+        }
+
+        //updating Data into db
+        public bool Update(eContactClass c)
+        {
+            bool isSuccess = false;
+
+
+            SqlConnection conn = new SqlConnection(ConnString);
+            try
+            {
+                string sql = "UPDATE CustomerDetails SET FirstName=@FirstName, LastName=@LastName,City=@City WHERE ContactNum=@ContactNum";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@FirstName", c.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", c.LastName);
+                cmd.Parameters.AddWithValue("@ContactNum", c.ContactNum);
+                cmd.Parameters.AddWithValue("@City", c.City);
+
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                    return isSuccess;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+
+            }
+            return isSuccess;
+        }
+
+        public bool Delete(eContactClass c)
+        {
+            bool isSuccess = false;
+
+
+            SqlConnection conn = new SqlConnection(ConnString);
+            try
+            {
+                string sql = "DELETE FROM CustomerDetails WHERE ContactNum=@ContactNum";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@ContactNum", c.ContactNum);
+                
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                    return isSuccess;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+
+            }
+            return isSuccess;
+        }
+
+    }
+}
+    
